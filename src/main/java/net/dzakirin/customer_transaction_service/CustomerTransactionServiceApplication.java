771b34ea1102799ace490c6630f03763ee9224c6
DZakirin <dzakirinmd@gmail.com>
2025-03-09 10:56:52 +0800

    feature: added configuration

---
+++ src/main/java/net/dzakirin/customer_transaction_service/CustomerTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package net.dzakirin.customer_transaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/dzakirin/customer_transaction_service/CustomerTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dzakirin.customer_transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
